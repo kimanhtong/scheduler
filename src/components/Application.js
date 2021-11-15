9be1af9581e22f310c775590d56be6ed94304183
Anh Tong <tongkimanh@yahoo.com>
2021-11-15 18:42:46 -0500

    Completed integrating static Appointment component to the scheduler

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,7 +1,9 @@
-import React, {useState} from "react";
+import React, {useState, Fragment} from "react";
 
 import "components/Application.scss";
 import DayList from "./DayList"
+import Appointment from "./Appointment/index";
+// import { render } from "@testing-library/react";
 
 const days = [
   {
@@ -21,8 +23,57 @@ const days = [
   },
 ];
 
-export default function Application() {
+const appointments = {
+  "1": {
+    id: 1,
+    time: "12pm",
+  },
+  "2": {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  "3": {
+    id: 3,
+    time: "2pm",
+  },
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
+
+export default function Application(props) {
   const [day, setDay] = useState('Monday');
+  const RenderedAppointment = Object.values(appointments).map(appointment => {
+    return (
+      <Appointment 
+        key={appointment.id} 
+        {...appointment} 
+      />
+    )
+  });
+
+  console.log(RenderedAppointment);
   return (
     <main className="layout">
       <section className="sidebar">
@@ -46,7 +97,10 @@ export default function Application() {
       />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        <Fragment>
+          {RenderedAppointment}
+          <Appointment key="last" time="5pm" />
+        </Fragment>
       </section>
     </main>
   );
